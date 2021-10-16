fd16a63a1673a65ee386917e9d4c87cd315ba346
ashutosh887 <ashutoshj887@gmail.com>
2021-10-16 23:30:09 +0530

    Miscellaneous 1

---
+++ 10-Misc1.py
@@ -0,0 +1,21 @@
+# Initializing multiple values altogether
+x, y, z = 2, 4, 6
+print(x, y, z)
+
+name = "Ashutosh"
+surname = "Jha"
+print(("My name is {} {}").format(name, surname))
+print(("My name reversed is {1} {0}").format(name, surname))
+
+# Operators:
+# Exponentiation: x**y
+# Indntity
+    # x is y
+    # x is not y
+# Membership
+    # x in y
+    # x not in y
+
+# Division:
+    # 3/2 = 1.5
+    # 3//2 = 1
